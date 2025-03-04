1f329291fff19652d24db2889949bc1db6592b3f
Jamari556 <Jamari.Greene25@compscihigh.org>
2025-03-04 16:09:29 +0000

    done

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html")
 })
 
+app.get("/home", (req, res) => {
+  res.sendFile(__dirname + "/public/bhm.html")
+})
+
+app.get("/leaders", (req, res) => {
+  res.sendFile(__dirname + "/public/leaders.html")
+})
+
+
 app.listen(3000, ()=>{
   console.log("Server started...")
 })
